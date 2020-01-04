ad0348c38bf044a5f9f1c8ca06d08bd3e02a689f
louis lafuma <louis.lafuma@ensiie.fe>
2020-01-04 15:37:21 +0100

    add alarm trigger
    ajout d'une fonction (setAlarmManager) déclenchant une fonction (receiver) à une date donnée

--- app/src/main/java/com/example/td2/TasksFragment.kt
+++ app/src/main/java/com/example/td2/TasksFragment.kt
@@ -2,13 +2,12 @@ package com.example.td2
 
 
 import android.R.attr.name
-import android.app.AlertDialog
-import android.app.DatePickerDialog
-import android.app.Dialog
-import android.app.TimePickerDialog
-import android.content.ClipDescription
-import android.content.DialogInterface
+import android.app.*
+import android.content.*
+import android.content.Context.ALARM_SERVICE
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +15,9 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -28,6 +30,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
+
 class TasksFragment : Fragment() {
 
     private val tasksRepository = TasksRepository()
@@ -38,6 +41,8 @@ class TasksFragment : Fragment() {
 //    ).toMutableList()
     private val tasks = mutableListOf<Task>()
     private val adapter = TasksAdapter(tasks)
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -63,6 +68,30 @@ class TasksFragment : Fragment() {
     }
 
     private val coroutineScope = MainScope()
+
+
+
+    fun setAlarmManager(date : String){
+        var cal = Calendar.getInstance()
+        /*cal.set(Calendar.YEAR, 2020)
+        cal.set(Calendar.MONTH, 0)
+        cal.set(Calendar.DAY_OF_MONTH, 4)
+        cal.set(Calendar.HOUR_OF_DAY, 15)
+        cal.set(Calendar.MINUTE, 8)
+        cal.set(Calendar.SECOND,0)*/
+        val myFormat = "MM/dd/yyyy-HH:mm"//format de la date et de l'heure. L'heure est fixée dans le dialogue de l'heure.
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        cal.setTime(sdf.parse(date))
+        cal.set(Calendar.SECOND,0)
+        val alarmManager = activity?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, Receiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        Log.d("MainActivity", " Create : " + Date().toString() + "cal date :" + sdf.format(cal.getTime()))
+        //la ligne ci-dessous envoie une alamre (execute la fonction receive) à la date de cal
+        //alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.timeInMillis, pendingIntent)
+        //envoie une alarme 10 secondes après l'appel de setAlarmManager
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10000, pendingIntent)
+    }
 
     fun createTask(title: String, description: String) {
         val task = Task("null", title, description)
@@ -100,6 +129,7 @@ class TasksFragment : Fragment() {
             val myFormat = "MM/dd/yyyy-HH:mm"//format de la date et de l'heure. L'heure est fixée dans le dialogue de l'heure.
             val sdf = SimpleDateFormat(myFormat, Locale.US)
             createTask(editTitle.text.toString(), sdf.format(cal.getTime()) + "\n" + editDescription.text.toString())
+            setAlarmManager(sdf.format(cal.getTime()))
         }, year, month, day)
 
         //Crée le dialogue de l'heure, c'est lui qui va lancer le dialogue de la date.
@@ -120,5 +150,43 @@ class TasksFragment : Fragment() {
         buttonCancel.setOnClickListener{ dialog.cancel() }
 
         dialog.show()
+    }
+
+    class Receiver : BroadcastReceiver() {
+        /*test de notification qui marche pas
+        fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean, name: String, description: String) {
+            // 1
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+                // 2
+                val channelId = "${context.packageName}-$name"
+                val channel = NotificationChannel(channelId, name, importance)
+                channel.description = description
+                channel.setShowBadge(showBadge)
+
+                // 3
+                val notificationManager = context.getSystemService(NotificationManager::class.java)
+                notificationManager.createNotificationChannel(channel)
+            }
+        }*/
+        //fonction qui est déclenchée par l'alarme, c'est ce qui va être lacé 5 minutes avant l'échéance de la tache
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d("MainActivity", " Receiver : " + Date().toString())
+            /* test de création de notification qui ne marche pas
+            createNotificationChannel(context!!,
+                NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
+                "test", "App notification channel.")
+            val channelId = "${context?.packageName}-${context?.getString(R.string.app_name)}"
+            var builder = NotificationCompat.Builder(context!!, channelId)
+                .setSmallIcon(R.drawable.roundedrectangle)
+                .setContentTitle("TestNotif")
+                .setContentText("ca va marcher")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            // 1
+            val notificationManager = NotificationManagerCompat.from(context)
+            // 2
+            notificationManager.notify(1001, builder.build())*/
+        }
     }
 }
